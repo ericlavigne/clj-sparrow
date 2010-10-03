bcd57c6c4d70be9d1a20afd7dacf939a7be96359
Eric Lavigne <lavigne.eric@gmail.com>
2010-10-03 12:48:59 -0400

    Created new Leiningen project, added description to the README, and added expected dependencies to project.clj.

---
+++ README.md
@@ -0,0 +1,17 @@
+# clj-sparrow
+
+Gentle introduction to Clojure web development
+
+## Usage
+
+FIXME: write
+
+## Installation
+
+Add [clj-sparrow "0.1"] to the dependencies section of your project.clj file.
+
+## License
+
+Copyright (C) 2010 Eric Lavigne
+
+Distributed under the Eclipse Public License, the same as Clojure.
---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject clj-sparrow "0.1"
+  :description "Gentle introduction to Clojure web development"
+  :url "http://github.com/ericlavigne/clj-sparrow"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [ring/ring-core "0.3.1"]
+                 [ring/ring-devel "0.3.1"]
+                 [ring/ring-jetty-adapter "0.3.1"]
+                 [compojure "0.5.2"]
+                 [hiccup "0.2.6"]
+                 [fleetdb "0.3.1"]
+                 [lein-run "1.0.0"]]
+  :dev-dependencies [[lein-clojars "0.6.0"]])
